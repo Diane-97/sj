5add8047d5188482cac574cae8174aaac873cfac
James Mashaka <jamesmashaka12@gmail.com>
2020-10-06 11:04:23 +0300

    changes

--- resources/views/viewanswerpage.blade.php
+++ resources/views/viewanswerpage.blade.php
@@ -19,12 +19,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <div class="wrapper">
 
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+    <nav class="main-header navbar navbar-expand-sm navbar-light navbar-white sticky-top">
         <div class="container">
             <a href="#" class="navbar-brand">
                 <img src="/assets/dist/img/logo.png" alt="SJ Logo" class="brand-image img-circle elevation-3"
                      style="opacity: .8">
-                <!--                <span class="brand-text font-weight-bold">S&J</span>-->
             </a>
 
             <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -35,13 +34,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <!-- Left navbar links -->
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Home</a>
+                        <a href="{{route('home')}}" class="nav-link">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">About Us</a>
+                        <a href="{{route('about')}}" class="nav-link">About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Contact Us</a>
+                        <a href="{{route('contact.index')}}" class="nav-link">Contact Us</a>
                     </li>
 
                 </ul>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,12 +19,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <div class="wrapper">
 
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+    <nav class="main-header navbar navbar-expand-md navbar-light navbar-white sticky-top">
         <div class="container">
             <a href="#" class="navbar-brand">
                 <img src="assets/dist/img/logo.png" alt="SJ Logo" class="brand-image img-circle elevation-3"
                      style="opacity: .8">
-                <!--                <span class="brand-text font-weight-bold">S&J</span>-->
             </a>
 
             <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,7 +40,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         <a href="{{route('about')}}" class="nav-link">About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a href="/contact" class="nav-link">Contact Us</a>
+                        <a href="{{route('contact.index')}}" class="nav-link">Contact Us</a>
                     </li>
 
                 </ul>
